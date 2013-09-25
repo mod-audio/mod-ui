e55dbfcce561809cd13ce7b02ee8904f79f3a9a3
Luis Fagundes <lhfagundes@hacklab.com.br>
2013-09-25 06:26:27 -0300

    development pedalboards removal script now has proper environment variables

--- scripts/device_remove_pedalboards.py
+++ scripts/device_remove_pedalboards.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 import os, sys, shutil
+from os.path import join
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+DATA_DIR = join(ROOT, 'dados')
+
+os.environ['MOD_DEV_ENVIRONMENT'] = os.environ.get("MOD_DEV_ENVIRONMENT", '1')
+os.environ['MOD_DATA_DIR'] = DATA_DIR
+os.environ['MOD_PLUGIN_LIBRARY_DIR'] = join(DATA_DIR, 'lib')
+os.environ['MOD_KEY_PATH'] = join(ROOT, 'keys')
+os.environ['MOD_HTML_DIR'] = join(ROOT, 'html')
+os.environ['MOD_DEVICE_WEBSERVER_PORT'] = '8888'
+os.environ['MOD_PHANTOM_BINARY'] = join(ROOT, 'phantomjs-1.9.0-linux-x86_64/bin/phantomjs')
+os.environ['MOD_SCREENSHOT_JS'] = join(ROOT, 'screenshot.js')
 
 from mod import settings
 
