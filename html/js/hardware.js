34b5528129507e3a77fc6276ffbee6ec5dfa6715
Luis Fagundes <lhfagundes@hacklab.com.br>
2013-10-07 22:37:37 -0300

    addressing label now doesn't contain port name

--- html/js/hardware.js
+++ html/js/hardware.js
@@ -140,8 +140,11 @@ function HardwareManager(options) {
 	}
 	var gui = options.getGui(instanceId)
 	var pluginName = gui.effect.label || gui.effect.name
-	var portName = pluginName
-	if (port.symbol != ':bypass') portName += ' - ' + port.name
+	var portName
+	if (port.symbol == ':bypass')
+	    portName = pluginName
+	else
+	    portName = port.name
 
 	var min = form.find('input[name=min]').val(currentAddressing.minimum || port.minimum)
 	var max = form.find('input[name=max]').val(currentAddressing.maximum || port.maximum)
