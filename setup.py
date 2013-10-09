1bc8b57dcdf60b02dc1ccd93b919cd70779fa699
Luis Fagundes <lhfagundes@hacklab.com.br>
2013-10-09 17:53:06 -0300

    release v0.26

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name = 'mod',
-      version = '0.25.2',
+      version = '0.26',
       description = 'MOD',
       long_description = 'MOD - Musician Operated Device - User Interface server and libraries',
       author = "Hacklab and AGR",
