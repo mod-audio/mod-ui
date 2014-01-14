9be43b3ca02995bfd5efe18db0bf75f2781a8592
Ricardo Crudo <ricardo.crudo@gmail.com>
2014-01-14 18:31:04 -0200

    script to debug the host, replaying a given log

---
+++ host_debug.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys, tornado, time, socket
+
+#from mod.hmi import HMI
+#from mod.protocol import Protocol
+#from mod.settings import HMI_BAUD_RATE, HMI_SERIAL_PORT
+#from tornado import web, options
+
+s = socket.socket()
+s.connect(('localhost', 5555))
+#s.settimeout(0.2)
+
+fh = open(sys.argv[1])
+
+queue = []
+
+for line in fh:
+    sep = '[host] sending -> '
+    if sep in line:
+        queue.append(line.strip().split(sep)[1])
+
+
+if len(queue) == 0:
+    print "Nothing to send"
+    sys.exit(1)
+
+for msg in queue:
+    time.sleep(0.05)
+    print 'sending:', msg
+    s.send(msg)
+    resp = s.recv(1024)
+    if resp != '':
+        print 'resp:', resp
+
