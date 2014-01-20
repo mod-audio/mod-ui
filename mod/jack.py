c52af17781a5b157d50d9c4246752776415d6bb5
Luis Fagundes <lhfagundes@hacklab.com.br>
2014-01-20 14:23:02 -0200

    avoid error message in development environments without jack

--- mod/jack.py
+++ mod/jack.py
@@ -1,12 +1,16 @@
-import subprocess
+import subprocess, os
 from tornado.ioloop import IOLoop
+from mod.settings import DEV_HOST
 
 def change_jack_bufsize(size, callback):
+    ioloop = IOLoop.instance()
+    
+    if not command_exists('jack_bufsize') and DEV_HOST:
+        ioloop.add_callback(callback)
+        return
+
     proc = subprocess.Popen(['jack_bufsize','%d' % size],
                             stdout=subprocess.PIPE)
-    
-
-    ioloop = IOLoop.instance()
     
     def check(fileno, event):
         if proc.poll() is None:
@@ -15,3 +19,10 @@ def change_jack_bufsize(size, callback):
         callback()
 
     ioloop.add_handler(proc.stdout.fileno(), check, 16)
+
+def command_exists(cmd):
+    paths = os.environ['PATH'].split(':')
+    for path in paths:
+        if os.path.exists(os.path.join(path, cmd)):
+            return True
+    return False
