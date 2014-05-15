b10675853f843d97bfe1b4769e047f725fa259f6
Luis Fagundes <lhfagundes@hacklab.com.br>
2014-05-15 15:45:12 -0300

    addressings structure in control chain

--- mod/control_chain.py
+++ mod/control_chain.py
@@ -24,6 +24,10 @@ from tornado import ioloop
                [0, 0, 1, 2, True, u'Foot 3 (Tap Tempo)'],
                [0, 0, 1, 3, True, u'Foot 4 (Tap Tempo)'],
                [1, 0, 1, 0, True, 'Foot - Exp. 1']]}
+
+actuator: hwtype, hwid, acttype, actid -> url, channel, actid
+addressing_type -> mode + port_properties
+options renamed to scale_points
 """
 
 ERROR = 255
@@ -263,7 +267,7 @@ class PipeLine():
         """
         if self.timeout is not None:
             self.ioloop.remove_timeout(self.timeout)
-        self.timeout = self.ioloop.add_timeout(self.last_poll, self.process_next)        
+        self.timeout = self.ioloop.add_timeout(self.last_poll, self.process_next)
 
     def process_next(self):
         """
@@ -293,6 +297,9 @@ class PipeLine():
             self.schedule(self.process_next)
 
     def receive(self, msg):
+        """
+        Called by gateway. Handles a message and process next thing in queue
+        """
         self.handle(msg)
         self.process_next()
 
@@ -310,13 +317,15 @@ class PipeLine():
         self.output_queue.put((hwid, function, data))
         self.interrupt()
 
-class HardwareManager():
+class AddressingManager():
     """
     HardwareManager is responsible for managing the connections to devices, know which ones are online, the hardware ids and addressing ids.
     It translates the Control Chain protocol to proper calls to Session methods.
     """
-    class __init__(self, session):
-        self.session = session
+    class __init__(self, handler):
+        # Handler is the function that will receive (instance_id, symbol, value) updates
+        self.handle = handler
+
         self.pipeline = PipeLine(self.receive)
 
         # Store hardware data, indexed by hwid
@@ -330,10 +339,22 @@ class HardwareManager():
 
         # Store addressings data, indexed by hwid, addressing_id
         self.addressings = {}
-        # hwid, addressing_id, indexed by instanceId, symbol
+        # Addressings indexed by instanceId, symbol. The data stored is a tupple containing:
+        #  - a boolean indicating if the hardware is present
+        #  - hwid, addressing_id if so
+        #  - url, channel otherwise
         self.addressing_index = {}
         # Pointers used to choose free addressing ids, per hardware
-        self.current_addressing_id = {}
+        self.addressing_id_pointers = {}
+        # Addressings to devices that are not connected
+        # indexed by (url, channel) then (instance_id, port_id)
+        self.pending_addressings = {}        
+
+        # Callbacks to be called when answers to commands sent are received,
+        # indexed by (hwid, function)
+        self.callbacks = {}
+
+        self.ioloop = ioloop.IOLoop.instance()
 
         # Maps Control Chain function ids to internal methods
         self.dispatch_table = {
@@ -345,86 +366,283 @@ class HardwareManager():
             }
     
     def start(self):
+        """
+        Starts the engine
+        """
         self.pipeline.start()
-    def send(self, hwid, function, data=None):
+        self.ioloop.add_callback(self._callbacks_garbage_collection)
+
+    def send(self, hwid, function, data=None, callback=None):
+        """
+        Sends a message through the pipeline.
+        Stores the callback to handle responses later.
+        """
+        current_callback, tstamp = self.callbacks.get((hwid, function))
+        if current_callback is not None:
+            # There's already a callback for this hardware/function. This means that
+            # the previous message was not returned, let's consider this an error
+            # in previous communication
+            current_callback(False)
+        self.callbacks[(hwid, function)] = (callback, ioloop.IOLoop.time())
         self.pipeline.send(hwid, function, data)
+
+    def _callbacks_garbage_collection(self):
+        """
+        Checks for callbacks for messages that have timed out.
+        Call this callbacks with False result and cleans memory.
+        """
+        now = ioloop.IOLoop.time()
+        keys = self.callbacks.keys()
+        for key in keys:
+            callback, tstamp = self.callbacks[key]
+            if now - tsamp > RESPONSE_TIMEOUT:
+                callback(False)
+                del self.callbacks[key]
+        self.ioloop.add_timeout(now + RESPONSE_TIMEOUT, self._callbacks_garbage_collection)
 
     def receive(self, msg):
         """
-        Called by pipeline, will route message internally
+        Called by pipeline.
+        Routes message internally, including the callback, if any
         """
         self.hardware_tstamp[msg.origin] = ioloop.IOLoop.time()
-        self.dispatch_table[msg.function](msg.origin, msg.data)
+        try:
+            callback, tstamp = self.callbacks.pop((msg.origin, msg.function))
+            self.dispatch_table[msg.function](msg.origin, msg.data, callback)
+        except KeyError:
+            """
+            Either this is not a response from AddressingManager (device_connect and data_request,
+            for example) or the message has timed out.
+            In case of timeout, this call will probably raise an error, the receiving function may
+            either expect no callback to handle an error or this call will result in error.
+            """
+            self.dispatch_table[msg.function](msg.origin, msg.data)
     
     def _generate_hardware_id(self):
+        """
+        Gets a free hardware id
+        """
         start = self.hardware_id_pointer
-        while self.hardwares.get(self.current_id) is not None:
-            self.hardware_id_pointer = (self.hardware_id_pointer + 1) % 256
-            if self.hardware_id_pointer == start:
+        pointer = start
+        while self.hardwares.get(pointer) is not None:
+            pointer = (pointer + 1) % 256
+            if pointer == start:
                 return None # full!
-        return self.hardware_id_pointer
+        self.hardware_id_pointer = pointer
+        return pointer
+
+    def _generate_addressing_id(self, hwid):
+        """
+        Gets a free addressing id for this hardware
+        """
+        start = self.addressing_id_pointers[hwid]
+        pointer = start
+        while self.addressings[hwid].get(pointer) is not None:
+            pointer = (pointer + 1) % 256
+            if pointer == start:
+                return None # full!
+        self.addressing_id_pointers[hwid] = pointer
+        return pointer
 
     def device_connect(self, origin, data):
+        """
+        Receives a device_connect message.
+        Creates the proper hardware id and initializes the structures for it,
+        send the hardware id to device and asks for its description.
+        If this device is enabled, loads it in pipeline.
+        """
         url = data['url']
         channel = data['channel']
         logging.info("connection %s on %d" % (url, channel))
         hwid = self._generate_hardware_id()
         self.hardwares[hwid] = data
-        self.hwid_index[(url, channel)] = hwid
+        self.hardware_index[(url, channel)] = hwid
         self.hardware_tstamp[hwid] = ioloop.IOLoop.time()
+        self.addressings[hwid] = {}
+        self.addressing_id_pointers[hwid] = 0
         self.send(hwid, CONNECTION, data)
-        if not os.path.exists(self.get_driver_path(url)):
-            self.send(hwid, DEVICE_DESCRIPTOR)
-            return
+        self.send(hwid, DEVICE_DESCRIPTOR)
 
         if not self.hardware_is_installed(url, channel):
             return
 
         self.load_hardware(url, channel)
 
+        # TODO sends all pending addressings for this hardware
+
     def device_disconnect(self, hwid):
+        """
+        Cleans all data from a given hardware id. This will be used in timeouts.
+        """
         data = self.hardwares.pop(hwid)
+        # TODO copy all addressings to self.pending_addressings
         del self.hw_index[(data['url'], data['channel'])]
         del self.hardware_tstamp[hwid]
         self.unload_hardware(hwid)
 
     def get_driver_path(self, url):
+        """
+        Gets path to store the device description, as send by it.
+        """
         device_id = md5(url).hexdigest()
         return os.path.join(HARDWARE_DRIVER_DIR, device_id)
 
     def hardware_is_installed(self, url, channel):
+        """
+        Checks if a device is installed by user in this channel.
+        Otherwise, it's not considered in pipeline.
+        """
         device_id = md5(url).hexdigest()
         installation_path = os.path.join(KNOWN_HARDWARE_DIR, "%s_%d" % (device_id, channel))
         return os.path.exists(installation_path) and os.path.exists(self.get_driver_path())
 
     def load_hardware(self, url, channel):
-        hwid = self.hwid_index.get((url, channel))
+        """
+        Load a hardware in Pipeline.
+        """
+        hwid = self.hardware_index.get((url, channel))
         if hwid is None:
             return
         self.pipeline.add_hardware(hwid)
 
-        """
-        hardware_data = json.loads(open(self.get_driver_path(url)).read())
-        self.hardwares[hwid].update(hardware_data)
-        """
-
     def unload_hardware(self, hwid):
+        """
+        Unload hardware in Pipeline.
+        """
         self.pipeline.remove_hardware(hwid)
 
     def save_device_driver(self, hwid, data):
+        """
+        Save the device description send by the device.
+        """
         path = self.get_driver_path(data['url'])
         open(path, 'w').write(json.dumps(data))
-        
 
     def receive_device_data(self, hwid, data):
+        """
+        Handles updates sent by hardware. This is the result of a polling by Pipeline.
+        """
         self.data_request_seqs[hwid] = (self.data_request_seqs[hwid]+1) % 256
         # TODO handle updates
         for addressing_id in data.requests:
-            self.send_addressing_data(addressing_id)            
+            self.send_addressing_data(addressing_id)
 
+    def address(self, actuator, mode, instance_id, port_id, port_properties, label, value, 
+                minimum, maximum, default, steps, unit, scale_points, callback=None):
+        """
+        Addresses a control port to an actuator.
+        First check if the port is not already addressed somewhere else and unaddress
+        if that's the case.
+        """
+        data = {
+            'actuator_id': actuator['actuator_id'],
+            'chosen_mask': mode,
+            'addressing_id': addrid,
+            'port_mask': port_properties,
+            'label': label,
+            'value': value,
+            'minimum': minimum,
+            'maximum': maximum,
+            'default': default,
+            'steps': steps,
+            'unit': unit,
+            'scale_points': scale_points,
+            }
 
+        def do_address(result=True, msg=None):
+            # This will either be called immediately or after unaddressing
+            # this port from other actuator. So, do_address maybe a callback.
+            if not result:
+                return callback(False, msg)
 
+            hwkey = (actuator['url'], actuator['channel'])
+            hwid = self.hardware_index.get(hwkey)
+            if hwid is None:
+                # The hardware we want to address is to is not connected, 
+                # let's store this addressing to send when it's present
+                if not self.pending_addressings.get(hwkey):
+                    self.pending_addressings[hwkey] = {}
+                self.pending_addressings[hwkey][(instance_id, port_id)] = data
+                self.addressing_index[(instance_id, port_id)] = (False, actuator['url'], actuator['channel'])
+                self.ioloop.add_callback(lambda: callback(True))
+                return
 
-        
-        
-    
+            addrid = self._generate_addressing_id(hwid)
+            self.addressings[hwid][addrid] = (instance_id, port_id, addressing)
+            self.addressing_index[(instance_id, port_id)] = (True, hwid, addrid)
+            self.send(hwid, ADDRESSING, data, callback)
+
+        current = self.addressing_index.get((instance_id, port_id)):
+        if current is None:
+            do_address()
+        else:
+            self.unaddress_port(instance_id, port_id, do_address)
+
+    def confirm_addressing(self, origin, data, callback):
+        """
+        Receives a confirmation that the addressing occurred.
+        """
+        callback(True)
+
+    def unaddress(self, instance_id, port_id, callback):
+        """
+        Removes an addressing for the given control port.
+        """
+        current = self.addressing_index.get((instance_id, port_id))
+        if current is None:
+            # Non-existing addressing, error
+            self.ioloop.add_callback(lambda: callback(False))
+            return
+
+        def clean_addressing_structures(ok=True, msg=None):
+            if not ok:
+                return callback(False, msg)
+            addr = self.addressing_index.pop[(instance_id, port_id)]
+            if addr[0]:
+                # device is present
+                del self.addressings[addr[1]][addr[2]]
+            else:
+                # device is not present
+                del self.pending_addressings[(addr[1], addr[2])][(instance_id, port_id)]
+            callback(True, msg)
+            
+        if not current[0]:
+            # Addressed to a disconnected device, we just need to clean structures
+            self.ioloop.add_callback(clean_addressing_structures)
+            return
+
+        connected, hwid, addrid = current
+        self.send(hwid, UNADDRESSING, { 'addressing_id': addrid }, clean_addressing_structures)
+
+    def confirm_unaddressing(self, origin, data, callback):
+        """
+        Receives an unaddressing confirmation
+        """
+        callback(True)
+
+    def unaddress_many(self, addressings, callback):
+        """
+        Gets a list of (instance_id, port_id) and unadresses all of them
+        """
+        def unaddress_next(ok=True, msg=None):
+            if not ok:
+                return callback(False)
+            if len(addressings) == 0:
+                return callback(True)
+            instance_id, port_id = addressings.pop(-1)
+            self.unaddress(instance_id, port_id, unaddress_next)
+        unaddress_next()
+
+    def unaddress_instance(self, instance_id, callback):
+        """
+        Removes all addressings from an instance
+        """
+        addressings = [ x for x in self.addressing_index.keys() if x[0] == instance_id ]
+        self.unaddress_many(addressings, callback)
+
+    def unaddress_all(self, callback):
+        """
+        Removes all addressings
+        """
+        addressings = self.addressing_index.keys()
+        self.unaddress_many(addressings, callback)
