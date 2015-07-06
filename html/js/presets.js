6a9926be47267f656604c86a1a329f4cfabff795
Markus Schmidt <schmidt@boomshop.net>
2015-07-06 21:38:20 +0200

    Initial modPopout Some documentation Events

--- html/js/presets.js
+++ html/js/presets.js
@@ -1,4 +1,32 @@
+// SOME CONSTANTS
+MOD_ANCHOR_N  = "MOD_ANCHOR_N";
+MOD_ANCHOR_E  = "MOD_ANCHOR_E";
+MOD_ANCHOR_S  = "MOD_ANCHOR_S";
+MOD_ANCHOR_W  = "MOD_ANCHOR_W";
+MOD_ANCHOR_NE = "MOD_ANCHOR_NE";
+MOD_ANCHOR_SE = "MOD_ANCHOR_SE";
+MOD_ANCHOR_SW = "MOD_ANCHOR_SW";
+MOD_ANCHOR_NW = "MOD_ANCHOR_NW";
+
 JqueryClass("modIcon", {
+    // a simple icon wrapper for DIVs
+    // more for unification than for simplification
+    // the icons colour is determined automaticaly depending on parent
+    // elements classes. If any parent has mod-dark or the element
+    // itself has mod-light, the icon is drawn as white.
+    //
+    // OPTIONS:
+    // 
+    // icons: define the icon type (look into img/icons/icons.css)
+    // size:  the icons size, default is 36 pixels
+    //
+    // EVENTS:
+    //
+    // iconset: is fired when the icon changes. argument is the
+    //          new icon type
+    // sizeset: is fired when the size is set. argument is the
+    //          new size
+    
     init: function (options) {
         options = $.extend({
             icon: "",
@@ -20,17 +48,46 @@ JqueryClass("modIcon", {
         if (icon)
             self.addClass("mod-icon-" + icon);
         self.data("icon").icon = icon;
+        this.trigger("iconset", [icon]);
         return self;
     },
     setSize: function (size) {
         var self = $(this);
         self.css({ width: size + "px", height: size + "px" });
         self.data("icon").size = size;
+        self.trigger("setsize", [size]);
         return self;
     },
 });
 
 JqueryClass('modButton', {
+    // a button with icon and/or text
+    // the fun part is its internal confirmation (e.g. for overwrite
+    // or delete actions) - the first click activates confirmation so
+    // the button changes its style, icon and label. clicking again
+    // fires the "action" event, clicking anywhere else resets
+    // confirmation
+    //
+    // OPTIONS:
+    //
+    // icon:     if the button has an icon, name the style (empty string
+    //           for no label)
+    // label;    the buttons label (empty string for no label)
+    // tooltip:  tooltip for the button (default HTML title tag)
+    // confirm:  bool if confirmation is required before firing event
+    // question: the buttons label for confirmation state
+    // 
+    // EVENTS:
+    //
+    // action:     is fired when the button is clicked once or twice
+    //             depending on the confirm option
+    // iconset:    is fired when the icon changes. argument is the
+    //             new icon type
+    // labelset:   is fired when the label changes. argument is the
+    //             new label
+    // tooltipset: is fired when the tooltip is set. argument is the
+    //             new tooltip
+    
     init: function (options) {
         var self = $(this);
         options = $.extend({
@@ -38,7 +95,6 @@ JqueryClass('modButton', {
             label: "",
             tooltip: "",
             confirm: false,
-            action: function (e) { },
             question: "Shure?"
         }, options);
         self.data("button", options);
@@ -54,6 +110,7 @@ JqueryClass('modButton', {
         var self = $(this);
         self.data("button").label = label;
         self.text(label);
+        this.trigger("labelset", [label]);
         return self;
     },
     setIcon: function (icon) {
@@ -65,10 +122,14 @@ JqueryClass('modButton', {
         else
             self.data("icon", null);
         self.data("button").icon = icon;
+        this.trigger("iconset", [icon]);
         return self;
     },
     setTooltip: function (tt) {
-        $(this).attr("title", tt);
+        var self = $(this);
+        self.attr("title", tt);
+        self.trigger("tooltipset", [tt]);
+        return self;
     },
     clicked: function (e) {
         var self = $(this);
@@ -86,9 +147,95 @@ JqueryClass('modButton', {
             e.stopPropagation();
             return;
         }
-        options.action(e);
+        self.trigger("action", [e]);
     },
 })
+
+JqueryClass("modEditable", {
+    // editable has a popout containing a text input. Hitting Enter or
+    // clicking the OK button fires the action which is called with
+    // entered text and JS event as arguments. Clicking anywhere else
+    // hides the poput without any action
+    //
+    // OPTIONS:
+    //
+    // title: the text of the element
+    // 
+    // EVENTS:
+    // 
+    // changed:  is fired when the user edited the text and hit enter
+    //           or clicked the OK button
+    init: function (options) {
+        var self = $(this);
+        options = $.extend({
+            title: "",
+        }, options);
+        self.click(self.clicked);
+        self.data("options", options);
+        return self;
+    },
+    setTitle: function (title) {
+        $(this).data("options").title = title;
+        self.text(title);
+        this.trigger("changed", [icon]);
+        return self;
+    },
+    clicked: function (e) {
+        
+    }
+});
+
+JqueryClass("modPopout", {
+    // poput is a container showing custom content bound to a specific
+    // element. the popout appears directly attached to $(this).
+    // 
+    // OPTIONS:
+    // 
+    // content: a JQuery element to display in the popout
+    // anchor:  a MOD_ANCHOR_X constant to define the poputs position
+    // auto:    a bool determining if the popout is positioned
+    //          automatically if it doesn't fit into the screen
+    // size:    a bool if the poput is resized to its parents size
+    //          depending on its anchor position (width or height)
+    //
+    // EVENTS:
+    //
+    // popout: is fired when the poput is shown
+    // popoff: is fired when the poput disapears
+    
+    init: function (options) {
+        var self = $(this);
+        options = $.extent({
+            content: null,
+            anchor: MOD_ANCHOR_S,
+            auto: true,
+            size: true
+        }, options);
+        var e = {};
+        e.popout = $("<div class='mod-popout'/>");
+        self.data("options", options);
+        self.data("elements", e);
+    },
+    show: function () {
+        var self = $(this);
+        var o = self.data("options");
+        var p = self.data("elements").popout;
+        switch (o.anchor) {
+            case default:
+            case MOD_ANCHOR_S:
+                if (o.size) p.outerWidth(self.outerWidth());
+                break;
+            // TODO: IMPLEMENT ALL ANCHOR POSITIONS AND AUTO POSITIONING
+        this.trigger("popout");
+        return self;
+    },
+    hide: function () {
+        var self = $(this);
+        self.data("element").popout.fadeOut();
+        self.trigger("popoff");
+        return self;
+    }
+});
 
 JqueryClass("presetManager", {
     init: function (options) {
@@ -110,20 +257,17 @@ JqueryClass("presetManager", {
         e.bind.modButton({
             icon: "bind",
             tooltip: "Bind the preset list to a controller",
-            action: function (e) { self.presetManager("loadClicked", self, e); }
-        });
+        }).on("action", function (e) { self.presetManager("loadClicked", self, e); });
         e.load.modButton({
             icon: "load",
             label: "load",
             tooltip: "Load the selected preset",
-            action: function (e) { self.presetManager("bindClicked", self, e); }
-        });
+        }).on("action", function (e) { self.presetManager("bindClicked", self, e); });
         e.save.modButton({
             icon: "save",
             label: "save",
             tooltip: "Save or overwrite the selected preset",
-            action: function (e) { self.presetManager("saveClicked", self, e); }
-        });
+        }).on("action", function (e) { self.presetManager("saveClicked", self, e); });
         
         self.addClass("preset-manager");
         
@@ -146,6 +290,7 @@ JqueryClass("presetManager", {
     clearPresets: function () {
         var self = $(this);
         self.data("elements").list.empty();
+        return self;
     },
     
     addPresets: function (presets) {
@@ -161,12 +306,14 @@ JqueryClass("presetManager", {
             });
             li.appendTo(self.data("elements").list);
         }
+        return self;
     },
     
     setPresets: function (presets) {
         var self = $(this);
         self.presetManager("clearPresets");
         self.presetManager("addPresets", presets);
+        return self;
     },
     
     presetClicked: function (prs, e) {
@@ -188,6 +335,7 @@ JqueryClass("presetManager", {
                 }
             }
         }
+        return self;
     },
     
     resetPresets: function() {
@@ -197,6 +345,7 @@ JqueryClass("presetManager", {
                 p.elements.title.text(p.name);
             }
         }
+        return self;
     },
     
     setPresetTitle: function(string) {
@@ -205,6 +354,7 @@ JqueryClass("presetManager", {
         if (string === false)
             string = "";
         $(this).data("elements").entry.val(string);
+        return self;
     },
     
     
@@ -216,26 +366,32 @@ JqueryClass("presetManager", {
     // event handlers
     deactivate: function(self, e) {
         
+        return self;
     },
     
     entryClicked: function(self, e) {
         console.log("click");
+        return self;
     },
     
     entryKeyup: function(self, e) {
         console.log("key");
+        return self;
     },
     
     loadClicked: function(self, e) {
         console.log("load");
+        return self;
     },
     
     saveClicked: function(self, e) {
         console.log("save");
+        return self;
     },
     
     bindClicked: function(self, e) {
         console.log("bind");
+        return self;
     },
 });
 
@@ -264,21 +420,19 @@ JqueryClass("presetEntry", {
         e.bind.modButton({
             icon: "bind",
             tooltip: "Bind the preset to a controller",
-            action: function (e) { options.bindPreset(e); }
-        });
+        }).on("action",function (e) { options.bindPreset(e); });
         e.edit.modButton({
             icon: "edit",
             tooltip: "Edit the preset title",
-            action: function (e) { options.bindPreset(e); }
-        });
+        }).on("action", function (e) { options.bindPreset(e); });
         e.remove.modButton({
             confirm: true,
             question: "",
             icon: "remove",
             tooltip: "Remove this preset",
-            action: function (e) {
+        }).on("action", function (e) {
                 options.removePreset(self, e);
-                self.remove(); }
+                self.remove();
         });
         
         self.addClass("mod-list-item");
@@ -300,9 +454,12 @@ JqueryClass("presetEntry", {
             self.removeClass("bound");
         }
         $(this).data("options").bind = bind;
+        return self;
     },
     setTitle: function (title) {
-        $(this).data("elements").title.text(title);
-        $(this).data("options").title = title;
+        var self = $(this);
+        self.data("elements").title.val(title);
+        self.data("options").title = title;
+        return self;
     },
 });
