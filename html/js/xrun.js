9747408cdbcb5f3e70eaa65accb310704ce1eceb
falkTX <falktx@gmail.com>
2015-07-29 15:55:00 +0200

    Misc

--- html/js/xrun.js
+++ html/js/xrun.js
@@ -1,16 +1,16 @@
 /*
  * Copyright 2012-2013 AGR Audio, Industria e Comercio LTDA. <contato@moddevices.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,10 +23,8 @@
 
         var timeout
         var poll = function () {
-            var url = '/sysmon/xrun/'
+            var url = '/jack/xrun/'
             var count = self.data('count')
-            if (count)
-                url += count
 
             $.ajax({
                 'url': url,
