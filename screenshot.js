6e2b0761513e01daf76e442297cbfd2f8e109ab5
falkTX <falktx@gmail.com>
2015-12-14 19:57:34 +0100

    Rework screenshot.js so that we wait for pedalboard to be ready

--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,38 @@
+/**
+ * Wait until the test condition is true or a timeout occurs. Useful for waiting
+ * on a server response or for a ui change (fadeIn, etc.) to occur.
+ *
+ * @param testFx javascript condition that evaluates to a boolean,
+ * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * as a callback function.
+ * @param onReady what to do when testFx condition is fulfilled,
+ * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * as a callback function.
+ * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
+ */
+function waitFor(testFx, onReady, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+                // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                    phantom.exit(1);
+                } else {
+                    // Condition fulfilled (timeout and/or condition is 'true')
+                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 250); //< repeat check every 250ms
+};
+
 var page = require('webpage').create(),
     system = require('system'),
     address, output, size;
@@ -12,15 +47,35 @@ if (system.args.length != 5) {
     width = system.args[3];
     height = system.args[4];
     page.viewportSize = { width: width, height: height };
+
+    var resources = [];
+    page.onResourceRequested = function(request) {
+        resources[request.id] = request.stage;
+    };
+    page.onResourceReceived = function(response) {
+        resources[response.id] = response.stage;
+    };
+
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
             phantom.exit();
         } else {
-            window.setTimeout(function () {
+            waitFor(function() {
+                // check if all resources are loaded
+                for (var i in resources) {
+                    if (resources[i] != 'end') {
+                        return false;
+                    }
+                }
+                // also check if 'loading pedalboard' message is not visible
+                return page.evaluate(function() {
+                    return !$(".screen-disconnected").is(":visible");
+                });
+            }, function() {
                 page.render(output);
                 phantom.exit();
-            }, 700);
+            }, 10000);
         }
-    });
+    })
 }
