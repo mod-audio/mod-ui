ec174c451a320fca3dce553630afc79bd42b93c2
falkTX <falktx@gmail.com>
2016-01-10 01:45:56 +0000

    Make it possible to use old or new preset widget by switching a var

--- html/js/modgui.js
+++ html/js/modgui.js
@@ -36,6 +36,7 @@ var loadedSettings = {}
 var loadedCSSs = {}
 var loadedJSs = {}
 var isSDK = false
+var newPresetStyle = false
 
 function loadDependencies(gui, effect, callback) { //source, effect, bundle, callback) {
     var iconLoaded = true
@@ -368,78 +369,81 @@ function GUI(effect, options) {
 
             if (instance)
             {
-                self.settings.find('[mod-role=presets]').change(function () {
-                    var value = $(this).val()
-                    options.presetLoad(value)
-                })
+                if (newPresetStyle)
+                {
+                    var prmel = self.settings.find('.preset-manager')
+                    self.presetManager = prmel.presetManager({})
 
-                /*
-                var prmel = self.settings.find('.preset-manager')
-                self.presetManager = prmel.presetManager({})
-
-                self.presetManager.on("load", function (e, instance, options) {
-                    console.log("load", instance, options);
-                    $.ajax({
-                        url: '/effect/preset/load/' + instance,
-                        data: {
-                            uri: options.uri
-                        },
-                        success: function (resp) {
-                            self.presetManager.presetManager("setPresetName", options.label)
-                        },
-                        error: function () {
-                        },
-                        cache: false,
-                        dataType: 'json'
+                    self.presetManager.on("load", function (e, instance, options) {
+                        console.log("load", instance, options);
+                        $.ajax({
+                            url: '/effect/preset/load/' + instance,
+                            data: {
+                                uri: options.uri
+                            },
+                            success: function (resp) {
+                                self.presetManager.presetManager("setPresetName", options.label)
+                            },
+                            error: function () {
+                            },
+                            cache: false,
+                            dataType: 'json'
+                        })
                     })
-                })
-                self.presetManager.on("save", function (e, instance, name, options) {
-                    console.log("save", instance, name, options)
-                    $.ajax({
-                        url: '/effect/preset/save/' + instance,
-                        data: {
-                            name: name
-                        },
-                        success: function (resp) {
-                            console.log(resp)
-                            if (resp.ok) {
-                                self.presetManager.presetManager("addPreset", {
-                                    name: name,
-                                    uri: resp.uri,
-                                    bind: false,
-                                    readonly: true,
-                                })
-                            }
-                        },
-                        error: function () {
-                        },
-                        cache: false,
-                        dataType: 'json'
+                    self.presetManager.on("save", function (e, instance, name, options) {
+                        console.log("save", instance, name, options)
+                        $.ajax({
+                            url: '/effect/preset/save/' + instance,
+                            data: {
+                                name: name
+                            },
+                            success: function (resp) {
+                                console.log(resp)
+                                if (resp.ok) {
+                                    self.presetManager.presetManager("addPreset", {
+                                        name: name,
+                                        uri: resp.uri,
+                                        bind: false,
+                                        readonly: true,
+                                    })
+                                }
+                            },
+                            error: function () {
+                            },
+                            cache: false,
+                            dataType: 'json'
+                        })
                     })
-                })
-                self.presetManager.on("rename", function (e, instance, name, options) {
-                    console.log("rename", instance, name, options)
-                })
-                self.presetManager.on("bind", function (e, instance, options) {
-                    console.log("bind", instance, options)
-                })
-                self.presetManager.on("bindlist", function (e, instance, options) {
-                    console.log("bindlist", instance, options)
-                })
+                    self.presetManager.on("rename", function (e, instance, name, options) {
+                        console.log("rename", instance, name, options)
+                    })
+                    self.presetManager.on("bind", function (e, instance, options) {
+                        console.log("bind", instance, options)
+                    })
+                    self.presetManager.on("bindlist", function (e, instance, options) {
+                        console.log("bindlist", instance, options)
+                    })
 
-                // bind: MOD_BIND_NONE, MOD_BIND_MIDI, MOD_BIND_KNOB, MOD_BIND_FOOTSWITCH or false
-                var p, _presets = []
-                for (var i in effect.presets) {
-                    p = effect.presets[i]
-                    _presets.push({
-                        name: p.label,
-                        uri: p.uri,
-                        bind: false,
-                        readonly: true,
+                    // bind: MOD_BIND_NONE, MOD_BIND_MIDI, MOD_BIND_KNOB, MOD_BIND_FOOTSWITCH or false
+                    var p, _presets = []
+                    for (var i in effect.presets) {
+                        p = effect.presets[i]
+                        _presets.push({
+                            name: p.label,
+                            uri: p.uri,
+                            bind: false,
+                            readonly: true,
+                        })
+                    }
+                    self.presetManager.presetManager("setPresets", instance, _presets)
+                }
+                else
+                {
+                    self.settings.find('[mod-role=presets]').change(function () {
+                        var value = $(this).val()
+                        options.presetLoad(value)
                     })
                 }
-                self.presetManager.presetManager("setPresets", instance, _presets)
-                */
             }
             else
             {
@@ -801,6 +805,8 @@ function GUI(effect, options) {
             // this is expensive and only useful for mod-sdk
             DEBUG = JSON.stringify(data, undefined, 4)
         }
+
+        data.newPresetStyle = newPresetStyle
 
         return data
     }
