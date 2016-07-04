aacf55e9000fe23f8fb234a9bc8e8579a8de30d8
falkTX <falktx@gmail.com>
2016-07-04 16:27:45 +0200

    Don't cache calls to authenticate device

--- html/js/desktop.js
+++ html/js/desktop.js
@@ -361,16 +361,16 @@ function Desktop(elements) {
         $.ajax({
             method: 'GET',
             url: SITEURL + '/devices/nonce',
-            dataType: 'json',
+            cache: false,
             success: function (resp) {
                 if (!resp || !resp.nonce) {
                     callback(false);
                     return;
                 }
-
                 $.ajax({
                     url: '/auth/nonce',
                     type: 'POST',
+                    cache: false,
                     contentType: 'application/json',
                     dataType: 'json',
                     data: JSON.stringify(resp),
@@ -385,6 +385,7 @@ function Desktop(elements) {
                         $.ajax({
                             url: SITEURL + '/devices/tokens',
                             type: 'POST',
+                            cache: false,
                             contentType: 'application/json',
                             dataType: 'json',
                             data: JSON.stringify(resp),
@@ -393,10 +394,10 @@ function Desktop(elements) {
                                     callback(false);
                                     return;
                                 }
-
                                 $.ajax({
                                     url: '/auth/token',
                                     type: 'POST',
+                                    cache: false,
                                     contentType: 'application/json',
                                     dataType: 'json',
                                     data: JSON.stringify(resp),
