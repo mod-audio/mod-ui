96e419102b924a6f5cf598df05e0dfc940151c41
sauklys <assauklys@gmail.com>
2017-04-13 03:52:09 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ os.environ['MOD_DEV_ENVIRONMENT'] = os.environ.get("MOD_DEV_ENVIRONMENT", '1')
 os.environ['MOD_DATA_DIR'] = DATA_DIR
 os.environ['MOD_LOG'] = os.environ.get("MOD_LOG", '1')
 os.environ['MOD_KEY_PATH'] = join(ROOT, 'keys')
-os.environ['MOD_DEVICE_WEBSERVER_PORT'] = '8888'
+os.environ['MOD_DEVICE_WEBSERVER_PORT'] = os.environ.get("MOD_DEVICE_WEBSERVER_PORT", '8888')
 os.environ['MOD_HTML_DIR'] = join(ROOT, 'html')
 os.environ['MOD_DEFAULT_PEDALBOARD'] = join(ROOT, 'default.pedalboard')
 
