c057d530a921b3bee3d42f46326635a8b25a07c2
Luis Fagundes <lhfagundes@hacklab.com.br>
2017-10-18 17:25:18 -0200

    use doap:license to detect if a plugin is commercial and sold by MOD Devices

--- mod/plugin_licenses.py
+++
@@ -1,9 +0,0 @@
-shopify_ids_hack = {
-    'http://code.google.com/p/amsynth/amsynth': True,
-}
-
-def get_plugins_licenses(plugins):
-    for plugin in plugins:
-        if shopify_ids_hack.get(plugin['uri']):
-            plugin['licensed'] = True
-    return plugins
