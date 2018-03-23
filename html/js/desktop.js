dddc04483f2763998a2702e72e59d804277433d0
Luis Fagundes <lhfagundes@gmail.com>
2018-03-23 16:14:11 -0300

    Tooltips on main menu

--- html/js/desktop.js
+++ html/js/desktop.js
@@ -1191,6 +1191,10 @@ function Desktop(elements) {
 	})
 
 	elements.settingsIcon.statusTooltip()
+	elements.pedalboardTrigger.statusTooltip()
+	elements.pedalboardBoxTrigger.statusTooltip()
+	elements.bankBoxTrigger.statusTooltip()
+	elements.cloudPluginBoxTrigger.statusTooltip()
 
     this.upgradeWindow = elements.upgradeWindow.upgradeWindow({
         icon: elements.upgradeIcon,
@@ -1826,7 +1830,7 @@ JqueryClass('statusTooltip', {
     init: function () {
         var self = $(this)
         var tooltip = $('<div class="tooltip">').appendTo($('body'))
-        $('<div class="arrow">').appendTo(tooltip)
+        tooltip.data('arrow', $('<div class="arrow">').appendTo(tooltip))
         $('<div class="text">').appendTo(tooltip)
         tooltip.hide()
         self.data('tooltip', tooltip)
@@ -1868,9 +1872,15 @@ JqueryClass('statusTooltip', {
             return
         var tooltip = self.data('tooltip')
         tooltip.find('.text').html(self.data('message'))
+		console.log(tooltip.left)
         tooltip.show().stop().animate({
             opacity: 1
         }, 200)
+        if (tooltip.position().left < 0) {
+            var arrow = tooltip.data('arrow');
+            arrow.css('left', arrow.position().left + tooltip.position().left);
+            tooltip.css('right', $(window).width() - self.position().left - self.width() + tooltip.position().left);
+        }
         if (timeout) {
             setTimeout(function () {
                 tooltip.stop().animate({
