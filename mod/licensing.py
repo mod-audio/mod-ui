c8b01585df672d9fdb7c7ee1555b640249f6dff1
Luis Fagundes <lhfagundes@gmail.com>
2018-05-10 15:53:28 -0300

    Revoke licenses that are not present in license list from the cloud

--- mod/licensing.py
+++ mod/licensing.py
@@ -6,14 +6,29 @@ LICENSE_INDEX = {}
 NEW_LICENSES = []
 
 def check_missing_licenses(licenses):
+    import ipdb; ipdb.set_trace()
+    # Compute a list of all licenses as revoking candidates
+    revoked = set()
+    for checksum in os.listdir(KEYS_PATH):
+        revoked.add(checksum)
+
+    # Check missing licenses and confirm the ones locally installed
     missing = []
     for plugin_license in licenses:
         uri = plugin_license['plugin_uri'].encode()
         LICENSE_INDEX[plugin_license['id']] = uri
         checksum = sha1(uri).hexdigest()
+        if checksum in revoked:
+            # license is still valid, let's unmark for revoking
+            revoked.remove(checksum)
         key_path = os.path.join(KEYS_PATH, checksum)
         if not os.path.exists(key_path):
             missing.append(plugin_license['id'])
+
+    # Delete revoked licenses
+    for checksum in revoked:
+        os.remove(os.path.join(KEYS_PATH, checksum))
+
     return missing
 
 def save_license(lid, data):
