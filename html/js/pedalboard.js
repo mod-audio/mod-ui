03ede9bbfac9d42010f010e59dacf66ac54fa8b3
Luis Fagundes <lhfagundes@gmail.com>
2018-05-29 17:41:33 -0300

    fix: there were two instances of windowManager, thus making some windows persist on closeWindows() calls (this is the right one, last one was a mistake)

--- html/js/pedalboard.js
+++ html/js/pedalboard.js
@@ -27,8 +27,6 @@ JqueryClass('pedalboard', {
             // wherever to skip zoom animations
             skipAnimations: false,
 
-            // WindowManager instance
-            windowManager: new WindowManager(),
             // HardwareManager instance, must be specified
             hardwareManager: null,
 
@@ -1373,7 +1371,6 @@ JqueryClass('pedalboard', {
 
             settings.window({
                 windowName: "Plugin Settings",
-                windowManager: self.data('windowManager')
             }).appendTo($('body'))
             icon.css({
                 'z-index': self.data('z_index'),
