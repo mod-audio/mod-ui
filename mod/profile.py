511569a28f3274261fa748c2ebd55e2b16d0c9f3
falkTX <falktx@falktx.com>
2019-10-10 05:15:18 +0100

    Save and load cv stuff in profile, fix duo values, cleanup
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod/profile.py
+++ mod/profile.py
@@ -22,14 +22,62 @@ def ensure_data_index_valid(data, fallback):
     if not isinstance(index, int) or index < 1 or index > Profile.NUM_PROFILES:
         data['index'] = fallback
 
-def fill_in_mixer_values(data):
+def apply_mixer_values(values, platform):
     if not os.path.exists("/usr/bin/mod-amixer"):
         return
-    data['input1volume']    = float(getoutput("/usr/bin/mod-amixer in 1 xvol").strip())
-    data['input2volume']    = float(getoutput("/usr/bin/mod-amixer in 2 xvol").strip())
-    data['output1volume']   = float(getoutput("/usr/bin/mod-amixer out 1 xvol").strip())
-    data['output1volume']   = float(getoutput("/usr/bin/mod-amixer out 2 xvol").strip())
-    data['headphoneVolume'] = float(getoutput("/usr/bin/mod-amixer hp xvol").strip())
+    if platform == "duo":
+        os.system("/usr/bin/mod-amixer in 1 dvol %f" % values['input1volume'])
+        os.system("/usr/bin/mod-amixer in 2 dvol %f" % values['input2volume'])
+        os.system("/usr/bin/mod-amixer out 1 dvol %f" % values['output1volume'])
+        os.system("/usr/bin/mod-amixer out 2 dvol %f" % values['output2volume'])
+        os.system("/usr/bin/mod-amixer hp dvol %f" % values['headphoneVolume'])
+        os.system("/usr/bin/mod-amixer hp byp %s" % Profile.value_to_string('headphoneBypass',
+                                                                            values['headphoneBypass']))
+        return
+    if platform == "duox":
+        os.system("/usr/bin/mod-amixer in 1 xvol %f" % values['input1volume'])
+        os.system("/usr/bin/mod-amixer in 2 xvol %f" % values['input2volume'])
+        os.system("/usr/bin/mod-amixer out 1 xvol %f" % values['output1volume'])
+        os.system("/usr/bin/mod-amixer out 2 xvol %f" % values['output2volume'])
+        os.system("/usr/bin/mod-amixer hp xvol %f" % values['headphoneVolume'])
+        os.system("/usr/bin/mod-amixer cvhp %s" % Profile.value_to_string('outputMode', values['outputMode']))
+        os.system("/usr/bin/mod-amixer cvexp %s" % Profile.value_to_string('inputMode', values['inputMode']))
+        os.system("/usr/bin/mod-amixer cvrange %s" % Profile.value_to_string('cvBias', values['cvBias']))
+        os.system("/usr/bin/mod-amixer exppedal %s" % Profile.value_to_string('expPedalMode', values['expPedalMode']))
+        return
+    if platform is None:
+        logging.error("[profile] apply_mixer_values called without platform")
+    else:
+        logging.error("[profile] apply_mixer_values called with unknown platform %s", platform)
+
+def fill_in_mixer_values(data, platform):
+    if not os.path.exists("/usr/bin/mod-amixer"):
+        return
+    if platform == "duo":
+        data['input1volume']    = float(getoutput("/usr/bin/mod-amixer in 1 dvol").strip())
+        data['input2volume']    = float(getoutput("/usr/bin/mod-amixer in 2 dvol").strip())
+        data['output1volume']   = float(getoutput("/usr/bin/mod-amixer out 1 dvol").strip())
+        data['output1volume']   = float(getoutput("/usr/bin/mod-amixer out 2 dvol").strip())
+        data['headphoneVolume'] = float(getoutput("/usr/bin/mod-amixer hp dvol").strip())
+        data['headphoneBypass'] = Profile.string_to_value('headphoneBypass',
+                                                          getoutput("/usr/bin/mod-amixer hp byp").strip())
+        return
+    if platform == "duox":
+        data['input1volume']    = float(getoutput("/usr/bin/mod-amixer in 1 xvol").strip())
+        data['input2volume']    = float(getoutput("/usr/bin/mod-amixer in 2 xvol").strip())
+        data['output1volume']   = float(getoutput("/usr/bin/mod-amixer out 1 xvol").strip())
+        data['output1volume']   = float(getoutput("/usr/bin/mod-amixer out 2 xvol").strip())
+        data['headphoneVolume'] = float(getoutput("/usr/bin/mod-amixer hp xvol").strip())
+        data['outputMode']      = Profile.string_to_value('outputMode', getoutput("/usr/bin/mod-amixer cvhp").strip())
+        data['inputMode']       = Profile.string_to_value('inputMode', getoutput("/usr/bin/mod-amixer cvexp").strip())
+        data['cvBias']          = Profile.string_to_value('cvBias', getoutput("/usr/bin/mod-amixer cvrange").strip())
+        data['expPedalMode']    = Profile.string_to_value('expPedalMode',
+                                                          getoutput("/usr/bin/mod-amixer exppedal").strip())
+        return
+    if platform is None:
+        logging.error("[profile] fill_in_mixer_values called without platform")
+    else:
+        logging.error("[profile] fill_in_mixer_values called with unknown platform %s", platform)
 
 # The user profile models environmental context.
 # That is all settings that are related to the physical hookup of the device.
@@ -41,11 +89,11 @@ class Profile(object):
     NUM_PROFILES = 4
     INTERMEDIATE_PROFILE_PATH = index_to_filepath(NUM_PROFILES + 1)
 
-    CONTROL_VOLTAGE_BIAS_0_to_5      = 0 # 0 to 5 volts
-    CONTROL_VOLTAGE_BIAS_m2d5_TO_2d5 = 1 # 2.5 to 2.5 volts
+    CONTROL_VOLTAGE_BIAS_m2d5_to_2d5 = 0 # 2.5 to 2.5 volts
+    CONTROL_VOLTAGE_BIAS_0_to_5      = 1 # 0 to 5 volts
 
-    EXPRESSION_PEDAL_MODE_TIP    = 0 # signal on tip
-    EXPRESSION_PEDAL_MODE_SLEEVE = 1 # signal on sleeve
+    EXPRESSION_PEDAL_MODE_RING = 0 # signal on ring/sleeve
+    EXPRESSION_PEDAL_MODE_TIP  = 1 # signal on tip
 
     INPUT_MODE_EXP_PEDAL = 0
     INPUT_MODE_CV        = 1
@@ -65,10 +113,11 @@ class Profile(object):
     TRANSPORT_SOURCE_ABLETON_LINK = 2
 
     DEFAULTS = {
-        'cvBias': CONTROL_VOLTAGE_BIAS_0_to_5,
-        'expressionPedalMode': EXPRESSION_PEDAL_MODE_TIP,
+        'cvBias': CONTROL_VOLTAGE_BIAS_m2d5_to_2d5,
+        'expPedalMode': EXPRESSION_PEDAL_MODE_RING,
+        'headphoneBypass': False,
         'headphoneVolume': -6.0, # 60%
-        'inputMode': INPUT_MODE_EXP_PEDAL,
+        'inputMode': INPUT_MODE_CV,
         'inputStereoLink': True,
         'masterVolumeChannelMode': MASTER_VOLUME_CHANNEL_MODE_BOTH,
         'midiChannelForPedalboardsNavigation': MIDI_CHANNEL_NAVIGATION_OFF,
@@ -109,10 +158,73 @@ class Profile(object):
         },
     }
 
-    def __init__(self, applyFn):
-        self.applyFn = applyFn
-        self.changed = False
-        self.values  = self.DEFAULTS.copy()
+    @classmethod
+    def string_to_value(cls, key, string):
+        if key == "headphoneBypass":
+            if string == "on":
+                return True
+            if string == "off":
+                return False
+        if key == "cvBias":
+            if string == "m2d5":
+                return cls.CONTROL_VOLTAGE_BIAS_m2d5_to_2d5
+            if string == "0to5":
+                return cls.CONTROL_VOLTAGE_BIAS_0_to_5
+        if key == "expPedalMode":
+            if string == "ring":
+                return cls.EXPRESSION_PEDAL_MODE_RING
+            if string == "tip":
+                return cls.EXPRESSION_PEDAL_MODE_TIP
+        if key == "inputMode":
+            if string == "cv":
+                return cls.INPUT_MODE_CV
+            if string == "exp":
+                return cls.INPUT_MODE_EXP_PEDAL
+        if key == "outputMode":
+            if string == "hp":
+                return cls.OUTPUT_MODE_HEADPHONE
+            if string == "cv":
+                return cls.OUTPUT_MODE_CV
+
+        logging.error("[profile] string_to_value called with invalid arg '%s' '%s'", key, string)
+        return cls.DEFAULTS.get(key, None)
+
+    @classmethod
+    def value_to_string(cls, key, value):
+        if key == "headphoneBypass":
+            if value == True:
+                return "on"
+            if value == False:
+                return "off"
+        if key == "cvBias":
+            if value == cls.CONTROL_VOLTAGE_BIAS_m2d5_to_2d5:
+                return "m2d5"
+            if value == cls.CONTROL_VOLTAGE_BIAS_0_to_5:
+                return "0to5"
+        if key == "expPedalMode":
+            if value == cls.EXPRESSION_PEDAL_MODE_RING:
+                return "ring"
+            if value == cls.EXPRESSION_PEDAL_MODE_TIP:
+                return "tip"
+        if key == "inputMode":
+            if value == cls.INPUT_MODE_CV:
+                return "cv"
+            if value == cls.INPUT_MODE_EXP_PEDAL:
+                return "exp"
+        if key == "outputMode":
+            if value == cls.OUTPUT_MODE_HEADPHONE:
+                return "hp"
+            if value == cls.OUTPUT_MODE_CV:
+                return "cv"
+
+        logging.error("[profile] value_to_string called with invalid arg '%s' '%s'", key, value)
+        return ""
+
+    def __init__(self, applyFn, hwdescriptor):
+        self.applyFn  = applyFn
+        self.platform = hwdescriptor.get("platform", None)
+        self.changed  = False
+        self.values   = self.DEFAULTS.copy()
 
         if os.path.exists(self.INTERMEDIATE_PROFILE_PATH):
             data = safe_json_load(self.INTERMEDIATE_PROFILE_PATH, dict)
@@ -125,7 +237,7 @@ class Profile(object):
             except IOError:
                 pass
 
-        fill_in_mixer_values(self.values)
+        fill_in_mixer_values(self.values, self.platform)
         ioloop.IOLoop.instance().add_callback(self.apply_first)
 
     # -----------------------------------------------------------------------------------------------------------------
@@ -168,7 +280,7 @@ class Profile(object):
         return self.values['cvBias']
 
     def get_exp_mode(self):
-        return self.values['expressionPedalMode']
+        return self.values['expPedalMode']
 
     def get_master_volume_channel_mode(self):
         return self.values['masterVolumeChannelMode']
@@ -199,34 +311,38 @@ class Profile(object):
     # setters
 
     def set_configurable_input_mode(self, value):
-        if value not in (self.INPUT_MODE_EXP_PEDAL, self.INPUT_MODE_CV):
+        if value not in (self.INPUT_MODE_CV, self.INPUT_MODE_EXP_PEDAL):
             logging.error("[profile] set_configurable_input_mode called with invalid value %s", value)
             return False
+        os.system("/usr/bin/mod-amixer cvexp %s" % self.value_to_string('inputMode', value))
         return self._compare_and_set_value('inputMode', value)
 
     def set_configurable_output_mode(self, value):
         if value not in (self.OUTPUT_MODE_HEADPHONE, self.OUTPUT_MODE_CV):
             logging.error("[profile] set_configurable_output_mode called with invalid value %s", value)
             return False
+        os.system("/usr/bin/mod-amixer cvhp %s" % self.value_to_string('outputMode', value))
         return self._compare_and_set_value('outputMode', value)
 
     def set_control_voltage_bias(self, value):
-        if value not in (self.CONTROL_VOLTAGE_BIAS_0_to_5, self.CONTROL_VOLTAGE_BIAS_m2d5_TO_2d5):
+        if value not in (self.CONTROL_VOLTAGE_BIAS_m2d5_to_2d5, self.CONTROL_VOLTAGE_BIAS_0_to_5):
             logging.error("[profile] set_control_voltage_bias called with invalid value %s", value)
             return False
+        os.system("/usr/bin/mod-amixer cvrange %s" % self.value_to_string('cvBias', value))
         return self._compare_and_set_value('cvBias', value)
 
     def set_exp_mode(self, value):
-        if value not in (self.EXPRESSION_PEDAL_MODE_TIP, self.EXPRESSION_PEDAL_MODE_SLEEVE):
+        if value not in (self.EXPRESSION_PEDAL_MODE_RING, self.EXPRESSION_PEDAL_MODE_TIP):
             logging.error("[profile] set_exp_mode called with invalid value %s", value)
             return False
-        return self._compare_and_set_value('expressionPedalMode', value)
+        os.system("/usr/bin/mod-amixer exppedal %s" % self.value_to_string('expPedalMode', value))
+        return self._compare_and_set_value('expPedalMode', value)
 
-    def set_headphone_volume(self, value):
-        if value < 0 or value > 100:
-            logging.error("[profile] set_headphone_volume called with invalid value %s", value)
+    def set_headphone_bypass(self, value):
+        if not isinstance(value, bool):
+            logging.error("[profile] set_headphone_bypass called with non-boolean value")
             return False
-        return self._compare_and_set_value('headphoneVolume', value)
+        return self._compare_and_set_value('headphoneBypass', value)
 
     def set_master_volume_channel_mode(self, value):
         if value not in (self.MASTER_VOLUME_CHANNEL_MODE_1,
@@ -272,14 +388,23 @@ class Profile(object):
             return False
         return self._compare_and_set_value('transportSource', value)
 
-    def set_tempo_bpb(self, bpb):
-        return self._compare_and_set_value('transportBPB', bpb)
+    def set_tempo_bpb(self, value):
+        if value < 1 or value > 16:
+            logging.error("[profile] set_tempo_bpb called with invalid value %s", value)
+            return False
+        return self._compare_and_set_value('transportBPB', value)
 
-    def set_tempo_bpm(self, bpm):
-        return self._compare_and_set_value('transportBPM', bpm)
+    def set_tempo_bpm(self, value):
+        if value < 20 or value > 240:
+            logging.error("[profile] set_tempo_bpm called with invalid value %s", value)
+            return False
+        return self._compare_and_set_value('transportBPM', value)
 
-    def set_send_midi_clk(self, onoff):
-        return self._compare_and_set_value('midiClockSend', onoff)
+    def set_send_midi_clk(self, value):
+        if not isinstance(value, bool):
+            logging.error("[profile] set_send_midi_clk called with non-boolean value")
+            return False
+        return self._compare_and_set_value('midiClockSend', value)
 
     # -----------------------------------------------------------------------------------------------------------------
     # persistent state
@@ -292,7 +417,7 @@ class Profile(object):
         self.values['index'] = index
 
         # request and store mixer values
-        fill_in_mixer_values(self.values)
+        fill_in_mixer_values(self.values, self.platform)
 
         # save intermediate file first
         with TextFileFlusher(self.INTERMEDIATE_PROFILE_PATH) as fh:
