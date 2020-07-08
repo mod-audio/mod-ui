494f2c784d3eff207f796cbe6211bd4eb7c8402c
Luis Fagundes <lhfagundes@gmail.com>
2020-07-08 09:10:00 -0300

    UI for cleanup in settings
    placeholder for backend code

--- mod/webserver.py
+++ mod/webserver.py
@@ -577,6 +577,24 @@ class SystemExeChange(JsonRequestHandler):
         lv2_cleanup()
         lv2_init()
 
+class SystemCleanup(JsonRequestHandler):
+    @gen.coroutine
+    def post(self):
+        banks = self.get_arguments('banks') == 'true'
+        pedalboards = self.get_arguments('pedalboards') == 'true'
+        plugins = self.get_arguments('plugins') == 'true'
+
+        if banks:
+            pass # placeholder
+
+        if pedalboards:
+            pass # placeholder
+
+        if plugins:
+            pass #placeholder
+
+        self.write(True)
+
 class UpdateDownload(MultiPartFileReceiver):
     destination_dir = "/tmp/os-update"
 
@@ -2024,6 +2042,7 @@ application = web.Application(
             (r"/system/info", SystemInfo),
             (r"/system/prefs", SystemPreferences),
             (r"/system/exechange", SystemExeChange),
+            (r"/system/cleanup", SystemCleanup),
 
             (r"/update/download/", UpdateDownload),
             (r"/update/begin", UpdateBegin),
