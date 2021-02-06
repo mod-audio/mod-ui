642cdd1ac618f59c77e72c15de92cad4dbb6ec24
falkTX <falktx@falktx.com>
2021-02-06 04:05:33 +0000

    Pass filetype to file list; Fix float-vector parse with negatives
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod/webserver.py
+++ mod/webserver.py
@@ -2149,6 +2149,7 @@ class FilesList(JsonRequestHandler):
                     retfiles.append({
                         'fullname': os.path.join(root, name),
                         'basename': name,
+                        'filetype': filetype,
                     })
 
         self.write({
