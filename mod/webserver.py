df54aebae46423428106526a4741f2ab14ae7f4c
falkTX <falktx@falktx.com>
2021-07-30 11:34:45 +0100

    Fix a typo
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod/webserver.py
+++ mod/webserver.py
@@ -256,7 +256,7 @@ class JsonRequestHandler(TimelessRequestHandler):
 
 class CachedJsonRequestHandler(JsonRequestHandler):
     def set_default_headers(self):
-        TimelessStaticFileHandler.set_default_headers(self)
+        JsonRequestHandler.set_default_headers(self)
         self.set_header("Cache-Control", "public, max-age=31536000")
         self.set_header("Expires", "Mon, 31 Dec 2035 12:00:00 gmt")
 
