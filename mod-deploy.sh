13c954522934944d65fc06f929b7e66761e87f25
falkTX <falktx@falktx.com>
2021-08-25 16:58:49 +0100

    Adjust deploy script, was missing some fonts

--- mod-deploy.sh
+++ mod-deploy.sh
@@ -21,26 +21,27 @@ ssh ${TARGET} mount / -o remount,rw
 
 ssh ${TARGET} rm -rf /usr/share/mod/html/css
 ssh ${TARGET} rm -rf /usr/share/mod/html/js
-ssh ${TARGET} mkdir -p /usr/share/mod/html/css/fontello/{css,font} /usr/share/mod/html/js/{lib/slick,utils}
+ssh ${TARGET} mkdir -p /usr/share/mod/html/css/fontello/{css,font} /usr/share/mod/html/js/{lib/slick/fonts,utils}
 
 ssh ${TARGET} rm -f  /usr/lib/python3.4/site-packages/mod/*.py*
 ssh ${TARGET} rm -f  /usr/lib/python3.4/site-packages/mod/communication/*.py*
 ssh ${TARGET} rm -f  /usr/lib/python3.4/site-packages/modtools/*.py*
 
-scp html/*.html                 ${TARGET}:/usr/share/mod/html/
-scp html/include/*.html         ${TARGET}:/usr/share/mod/html/include/
-scp html/resources/*.html       ${TARGET}:/usr/share/mod/html/resources/
-scp html/css/*.css              ${TARGET}:/usr/share/mod/html/css/
-scp html/css/fontello/css/*.css ${TARGET}:/usr/share/mod/html/css/fontello/css/
-scp html/css/fontello/font/*.*  ${TARGET}:/usr/share/mod/html/css/fontello/font/
-scp html/js/*.js                ${TARGET}:/usr/share/mod/html/js/
-scp html/js/lib/*.js            ${TARGET}:/usr/share/mod/html/js/lib/
-scp html/js/lib/slick/*.css     ${TARGET}:/usr/share/mod/html/js/lib/slick/
-scp html/js/lib/slick/*min.js   ${TARGET}:/usr/share/mod/html/js/lib/slick/
-scp html/js/utils/*.js          ${TARGET}:/usr/share/mod/html/js/utils/
-scp mod/*.py                    ${TARGET}:/usr/lib/python3.4/site-packages/mod/
-scp mod/communication/*.py      ${TARGET}:/usr/lib/python3.4/site-packages/mod/communication/
-scp modtools/*.py               ${TARGET}:/usr/lib/python3.4/site-packages/modtools/
+scp html/*.html                   ${TARGET}:/usr/share/mod/html/
+scp html/include/*.html           ${TARGET}:/usr/share/mod/html/include/
+scp html/resources/*.html         ${TARGET}:/usr/share/mod/html/resources/
+scp html/css/*.css                ${TARGET}:/usr/share/mod/html/css/
+scp html/css/fontello/css/*.css   ${TARGET}:/usr/share/mod/html/css/fontello/css/
+scp html/css/fontello/font/*.*    ${TARGET}:/usr/share/mod/html/css/fontello/font/
+scp html/js/*.js                  ${TARGET}:/usr/share/mod/html/js/
+scp html/js/lib/*.js              ${TARGET}:/usr/share/mod/html/js/lib/
+scp html/js/lib/slick/*.{css,gif} ${TARGET}:/usr/share/mod/html/js/lib/slick/
+scp html/js/lib/slick/*min.js     ${TARGET}:/usr/share/mod/html/js/lib/slick/
+scp html/js/lib/slick/fonts/*.*   ${TARGET}:/usr/share/mod/html/js/lib/slick/fonts/
+scp html/js/utils/*.js            ${TARGET}:/usr/share/mod/html/js/utils/
+scp mod/*.py                      ${TARGET}:/usr/lib/python3.4/site-packages/mod/
+scp mod/communication/*.py        ${TARGET}:/usr/lib/python3.4/site-packages/mod/communication/
+scp modtools/*.py                 ${TARGET}:/usr/lib/python3.4/site-packages/modtools/
 
 ssh ${TARGET} rm -rf /usr/lib/python3.4/site-packages/mod/__pycache__
 ssh ${TARGET} rm -rf /usr/lib/python3.4/site-packages/mod/communication/__pycache__
