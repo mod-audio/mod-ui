4fd8923848b0c10b6a4b5016f0bc17e051874d36
falkTX <falktx@falktx.com>
2023-03-22 18:39:34 +0100

    Fix tempo labels
    Signed-off-by: falkTX <falktx@falktx.com>

--- modtools/tempo.py
+++ modtools/tempo.py
@@ -1,7 +1,7 @@
 # List of all subdividers' 'value's and 'label's
 dividers = [{
     'value': 0.333,
-    'label': "2T"
+    'label': "2."
   },
   {
     'value': 0.5,
@@ -9,10 +9,10 @@ dividers = [{
   },
   {
     'value': 0.75,
-    'label': "2."
+    'label': "2T"
   },{
     'value': 0.666,
-    'label': "1T"
+    'label': "1."
   },
   {
     'value': 1,
@@ -20,11 +20,11 @@ dividers = [{
   },
   {
     'value': 1.5,
-    'label': "1."
+    'label': "1T"
   },
   {
     'value': 1.333,
-    'label': "1/2T"
+    'label': "1/2."
   },
   {
     'value': 2,
@@ -32,11 +32,11 @@ dividers = [{
   },
   {
     'value': 3,
-    'label': "1/2."
+    'label': "1/2T"
   },
   {
     'value': 2.666,
-    'label': "1/4T"
+    'label': "1/4."
   },
   {
     'value': 4,
@@ -44,11 +44,11 @@ dividers = [{
   },
   {
     'value': 6,
-    'label': "1/4."
+    'label': "1/4T"
   },
   {
     'value': 5.333,
-    'label': "1/8T"
+    'label': "1/8."
   },
   {
     'value': 8,
@@ -56,11 +56,11 @@ dividers = [{
   },
   {
     'value': 12,
-    'label': "1/8."
+    'label': "1/8T"
   },
   {
     'value': 10.666,
-    'label': "1/16T"
+    'label': "1/16."
   },
   {
     'value': 16,
@@ -68,11 +68,11 @@ dividers = [{
   },
   {
     'value': 24,
-    'label': "1/16."
+    'label': "1/16T"
   },
   {
     'value': 21.333,
-    'label': "1/32T"
+    'label': "1/32."
   },
   {
     'value': 32,
@@ -80,7 +80,7 @@ dividers = [{
   },
   {
     'value': 48,
-    'label': "1/32."
+    'label': "1/32T"
   }
 ]
 
