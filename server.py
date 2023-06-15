6baaa39296e19f66bf591383e09e8e4cbe2675f1
falkTX <falktx@falktx.com>
2023-06-15 11:49:35 +0200

    Allow to override MOD_DATA_DIR in dev server.py
    Signed-off-by: falkTX <falktx@falktx.com>

--- server.py
+++ server.py
@@ -4,7 +4,6 @@
 
 import os, sys
 from datetime import datetime
-from os.path import join
 from random import randint
 
 
@@ -29,20 +28,21 @@ def create_dummy_credentials():
             print('Can\'t create a device key: {0}'.format(ex))
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
-DATA_DIR = join(ROOT, 'data')
+DATA_DIR = os.environ.get("MOD_DATA_DIR", os.path.join(ROOT, 'data'))
+
 os.makedirs(DATA_DIR, exist_ok=True)
 
 os.environ['MOD_DEV_ENVIRONMENT'] = os.environ.get("MOD_DEV_ENVIRONMENT", '1')
 os.environ['MOD_DATA_DIR'] = DATA_DIR
 os.environ['MOD_LOG'] = os.environ.get("MOD_LOG", '1')
-os.environ['MOD_KEY_PATH'] = join(DATA_DIR, 'keys')
+os.environ['MOD_KEY_PATH'] = os.path.join(DATA_DIR, 'keys')
 os.environ['MOD_DEVICE_WEBSERVER_PORT'] = '8888'
-os.environ['MOD_HTML_DIR'] = join(ROOT, 'html')
-os.environ['MOD_DEFAULT_PEDALBOARD'] = join(ROOT, 'default.pedalboard')
-os.environ['MOD_DEVICE_KEY'] = join(DATA_DIR, 'rsa')
-os.environ['MOD_DEVICE_TAG'] = join(DATA_DIR, 'tag')
-os.environ['MOD_DEVICE_UID'] = join(DATA_DIR, 'uid')
-os.environ['MOD_API_KEY'] = join(DATA_DIR, 'mod_api_key.pub')
+os.environ['MOD_HTML_DIR'] = os.path.join(ROOT, 'html')
+os.environ['MOD_DEFAULT_PEDALBOARD'] = os.path.join(ROOT, 'default.pedalboard')
+os.environ['MOD_DEVICE_KEY'] = os.path.join(DATA_DIR, 'rsa')
+os.environ['MOD_DEVICE_TAG'] = os.path.join(DATA_DIR, 'tag')
+os.environ['MOD_DEVICE_UID'] = os.path.join(DATA_DIR, 'uid')
+os.environ['MOD_API_KEY'] = os.path.join(DATA_DIR, 'mod_api_key.pub')
 
 create_dummy_credentials()
 
