a8b3b7eab3813290a62648a5a5b6e1d70d80ff22
falkTX <falktx@falktx.com>
2023-07-08 14:00:43 +0200

    Use -O option for scp during deploy
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod-deploy.sh
+++ mod-deploy.sh
@@ -19,6 +19,7 @@ fi
 
 # needed since ssh rsa deprecation/breakage
 SSH_OPTIONS="-o PubkeyAcceptedAlgorithms=+ssh-rsa"
+SCP_OPTIONS="${SSH_OPTIONS} -O"
 
 ssh ${SSH_OPTIONS} ${TARGET} mount / -o remount,rw
 
@@ -30,23 +31,23 @@ ssh ${SSH_OPTIONS} ${TARGET} rm -f  /usr/lib/python3.4/site-packages/mod/*.py*
 ssh ${SSH_OPTIONS} ${TARGET} rm -f  /usr/lib/python3.4/site-packages/mod/communication/*.py*
 ssh ${SSH_OPTIONS} ${TARGET} rm -f  /usr/lib/python3.4/site-packages/modtools/*.py*
 
-scp ${SSH_OPTIONS} html/*.html                   ${TARGET}:/usr/share/mod/html/
-scp ${SSH_OPTIONS} html/include/*.html           ${TARGET}:/usr/share/mod/html/include/
-scp ${SSH_OPTIONS} html/resources/*.html         ${TARGET}:/usr/share/mod/html/resources/
-scp ${SSH_OPTIONS} html/css/*.css                ${TARGET}:/usr/share/mod/html/css/
-scp ${SSH_OPTIONS} html/css/fontello/css/*.css   ${TARGET}:/usr/share/mod/html/css/fontello/css/
-scp ${SSH_OPTIONS} html/css/fontello/font/*.*    ${TARGET}:/usr/share/mod/html/css/fontello/font/
-scp ${SSH_OPTIONS} html/js/*.js                  ${TARGET}:/usr/share/mod/html/js/
-scp ${SSH_OPTIONS} html/js/lib/*.js              ${TARGET}:/usr/share/mod/html/js/lib/
-scp ${SSH_OPTIONS} html/js/lib/slick/*.{css,gif} ${TARGET}:/usr/share/mod/html/js/lib/slick/
-scp ${SSH_OPTIONS} html/js/lib/slick/*min.js     ${TARGET}:/usr/share/mod/html/js/lib/slick/
-scp ${SSH_OPTIONS} html/js/lib/slick/fonts/*.*   ${TARGET}:/usr/share/mod/html/js/lib/slick/fonts/
-scp ${SSH_OPTIONS} html/js/utils/*.js            ${TARGET}:/usr/share/mod/html/js/utils/
-scp ${SSH_OPTIONS} html/img/*.png                ${TARGET}:/usr/share/mod/html/img/
-scp ${SSH_OPTIONS} html/img/*.svg                ${TARGET}:/usr/share/mod/html/img/
-scp ${SSH_OPTIONS} mod/*.py                      ${TARGET}:/usr/lib/python3.4/site-packages/mod/
-scp ${SSH_OPTIONS} mod/communication/*.py        ${TARGET}:/usr/lib/python3.4/site-packages/mod/communication/
-scp ${SSH_OPTIONS} modtools/*.py                 ${TARGET}:/usr/lib/python3.4/site-packages/modtools/
+scp ${SCP_OPTIONS} html/*.html                   ${TARGET}:/usr/share/mod/html/
+scp ${SCP_OPTIONS} html/include/*.html           ${TARGET}:/usr/share/mod/html/include/
+scp ${SCP_OPTIONS} html/resources/*.html         ${TARGET}:/usr/share/mod/html/resources/
+scp ${SCP_OPTIONS} html/css/*.css                ${TARGET}:/usr/share/mod/html/css/
+scp ${SCP_OPTIONS} html/css/fontello/css/*.css   ${TARGET}:/usr/share/mod/html/css/fontello/css/
+scp ${SCP_OPTIONS} html/css/fontello/font/*.*    ${TARGET}:/usr/share/mod/html/css/fontello/font/
+scp ${SCP_OPTIONS} html/js/*.js                  ${TARGET}:/usr/share/mod/html/js/
+scp ${SCP_OPTIONS} html/js/lib/*.js              ${TARGET}:/usr/share/mod/html/js/lib/
+scp ${SCP_OPTIONS} html/js/lib/slick/*.{css,gif} ${TARGET}:/usr/share/mod/html/js/lib/slick/
+scp ${SCP_OPTIONS} html/js/lib/slick/*min.js     ${TARGET}:/usr/share/mod/html/js/lib/slick/
+scp ${SCP_OPTIONS} html/js/lib/slick/fonts/*.*   ${TARGET}:/usr/share/mod/html/js/lib/slick/fonts/
+scp ${SCP_OPTIONS} html/js/utils/*.js            ${TARGET}:/usr/share/mod/html/js/utils/
+scp ${SCP_OPTIONS} html/img/*.png                ${TARGET}:/usr/share/mod/html/img/
+scp ${SCP_OPTIONS} html/img/*.svg                ${TARGET}:/usr/share/mod/html/img/
+scp ${SCP_OPTIONS} mod/*.py                      ${TARGET}:/usr/lib/python3.4/site-packages/mod/
+scp ${SCP_OPTIONS} mod/communication/*.py        ${TARGET}:/usr/lib/python3.4/site-packages/mod/communication/
+scp ${SCP_OPTIONS} modtools/*.py                 ${TARGET}:/usr/lib/python3.4/site-packages/modtools/
 
 ssh ${SSH_OPTIONS} ${TARGET} rm -rf /usr/lib/python3.4/site-packages/mod/__pycache__
 ssh ${SSH_OPTIONS} ${TARGET} rm -rf /usr/lib/python3.4/site-packages/mod/communication/__pycache__
