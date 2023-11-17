fc843bde9f0b23cfab40c3bcc133378342a8a070
falkTX <falktx@falktx.com>
2023-11-17 23:53:40 +0100

    crypto: compatibility with Cryptodome
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod/communication/crypto.py
+++ mod/communication/crypto.py
@@ -5,10 +5,18 @@
 import io
 import uuid
 
-from Crypto.Cipher import PKCS1_OAEP, AES
-from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_v1_5
-from Crypto.Hash import SHA1
+try:
+    from Cryptodome.Cipher import PKCS1_OAEP, AES
+    from Cryptodome.Hash import SHA1
+    from Cryptodome.PublicKey import RSA
+    from Cryptodome.Signature import pkcs1_15 as PKCS1_v1_5
+    usingCryptodome = True
+except ImportError:
+    from Crypto.Cipher import PKCS1_OAEP, AES
+    from Crypto.Hash import SHA1
+    from Crypto.PublicKey import RSA
+    from Crypto.Signature import PKCS1_v1_5
+    usingCryptodome = False
 
 
 def encrypt(recipient_key_txt: str, data: str):
@@ -24,20 +32,25 @@ def encrypt(recipient_key_txt: str, data: str):
 
     # Encrypt the data with the AES session keynonce: str, nonce: str,
     cipher_aes = AES.new(session_key, AES.MODE_EAX, uuid.uuid4().bytes)
-    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+    if usingCryptodome:
+        ciphertext, tag = cipher_aes.encrypt_and_digest(bytes(data, 'utf-8'))
+    else:
+        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
     [out.write(x) for x in (cipher_aes.nonce, tag, ciphertext)]
     out.seek(0)
     return out.getvalue()
 
 
 def decrypt(private_key_txt: str, encrypted: bytes):
-
     private_key = RSA.importKey(private_key_txt)
-    private_key_size = int((private_key.size() + 1)/8)
+    if usingCryptodome:
+        private_key_size = int((private_key.size_in_bits() + 1)/8)
+    else:
+        private_key_size = int((private_key.size() + 1)/8)
 
     buffer = io.BytesIO(encrypted)
     session_key, nonce, tag, ciphertext = \
-       [buffer .read(x) for x in (private_key_size, 16, 16, -1)]
+       [buffer.read(x) for x in (private_key_size, 16, 16, -1)]
 
     # Decrypt the session key with the public RSA key
     cipher_rsa = PKCS1_OAEP.new(private_key)
@@ -56,8 +69,15 @@ def sign_message_sha1(key_txt: str, message: str):
     return PKCS1_v1_5.new(key).sign(sha1)
 
 
-def verify_signature(sender_key_txt: str, contents: str, signature: str):
+def verify_signature(sender_key_txt: str, contents: str, signature: bytes):
     sender_key = RSA.importKey(sender_key_txt)
     sha1 = SHA1.new()
     sha1.update(contents.encode())
-    return PKCS1_v1_5.new(sender_key).verify(sha1, signature)
+    if usingCryptodome:
+        try:
+            PKCS1_v1_5.new(sender_key).verify(sha1, signature)
+            return True
+        except ValueError:
+            return False
+    else:
+        return PKCS1_v1_5.new(sender_key).verify(sha1, signature)
