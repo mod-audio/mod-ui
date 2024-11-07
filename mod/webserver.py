204b3fce3c1bcee870cd919849d06b64d3e6087b
falkTX <falktx@falktx.com>
2024-11-07 14:19:13 +0100

    Allow RemotePluginWebSocket connections from localhost:8010
    Signed-off-by: falkTX <falktx@falktx.com>

--- mod/webserver.py
+++ mod/webserver.py
@@ -1176,7 +1176,7 @@ class RemotePluginWebSocket(websocket.WebSocketHandler):
         protocol, domain = match.groups()
         if protocol not in ("http", "https"):
             return False
-        if domain != "mod.audio" and not domain.endswith(".mod.audio"):
+        if domain != "localhost:8010" and domain != "mod.audio" and not domain.endswith(".mod.audio"):
             return False
         return True
 
