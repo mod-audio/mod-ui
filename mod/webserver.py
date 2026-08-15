5d1279965fae763a9bbbf98ad5fde92d59b127d1
Mark Melvin <mark.melvin@gmail.com>
2026-08-15 16:21:05 -0400

    Only support .easyspinprog file extension to be unique

--- mod/webserver.py
+++ mod/webserver.py
@@ -2224,7 +2224,7 @@ class FilesList(JsonRequestHandler):
             return ("NAM Models", (".nam",))
 
         elif filetype == "easyspinprog":
-            return ("Easy Spin Programs", (".json",".bin",))
+            return ("Easy Spin Programs", (".easyspinprog",))
 
         else:
             return (None, ())
